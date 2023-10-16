9ada0a001994010fa238d4700be22f14906b85f7
Tipu1996 <tipu.solehria.com>
2023-10-16 21:00:10 +0300

    cleaned up default vite app + installed mui + set up light and dark mode + a header for turning them on and off

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { lightThemeOptions, darkThemeOptions } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+import Header from "./components/Header";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	const [theme, setTheme] = useState("dark");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+	const changeTheme = () => {
+		setTheme(theme === "light" ? "dark" : "light");
+	};
 
-export default App
+	return (
+		<ThemeProvider
+			theme={theme === "light" ? lightThemeOptions : darkThemeOptions}
+		>
+			<CssBaseline />
+			<Header theme={theme} changeTheme={changeTheme} />
+		</ThemeProvider>
+	);
+};
+
+export default App;
